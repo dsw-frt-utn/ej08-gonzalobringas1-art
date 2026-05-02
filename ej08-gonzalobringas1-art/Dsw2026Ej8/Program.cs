4c4fa74fd03c363b6599dae76ba97742ce57a89e
Bringas Gonzalo <gonzalo.bringas@alu.frt.utn.edu.ar>
2026-05-02 18:35:26 -0300

    problema 1 resuelto

---
+++ ej08-gonzalobringas1-art/Dsw2026Ej8/Program.cs
@@ -0,0 +1,19 @@
+﻿using SistemaProductos;
+
+namespace Dsw2026Ej8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+
+            ProductHelper helper = new ProductHelper();
+string etiqueta = helper.ObtenerEtiquetaProducto(10254, "Monitor Gamer 24'", 450000.50m);
+
+Console.WriteLine(etiqueta); 
+// Salida esperada: [10254] Monitor Gamer 24' - $450.000,50
+        }
+    }
+}
+
