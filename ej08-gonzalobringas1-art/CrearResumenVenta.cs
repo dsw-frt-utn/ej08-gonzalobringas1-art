f48a7a36c1bdec6e64acb4e7958e26f102a9d8e5
Bringas Gonzalo <gonzalo.bringas@alu.frt.utn.edu.ar>
2026-05-03 20:27:27 -0300

    Problema 2 Resuelto

---
+++ ej08-gonzalobringas1-art/CrearResumenVenta.cs
@@ -0,0 +1,17 @@
+public class Problema2
+{
+    public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice)
+    {
+       
+        var resumen = new 
+        {
+            Code = productCode,
+            Description = productDescription,
+            Quantity = quantity,
+            Total = quantity <= 0 ? 0 : (quantity * unitPrice) 
+        };
+
+        
+        return $"{resumen.Code}-{resumen.Description}-{resumen.Total}";
+    }
+}
