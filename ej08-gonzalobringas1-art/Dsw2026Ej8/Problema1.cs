63cc80622eb6cd13cc4841a3c9d63ef7b3d4b9c3
Bringas Gonzalo <gonzalo.bringas@alu.frt.utn.edu.ar>
2026-05-03 19:49:37 -0300

    Problema 3 realizado

--- ej08-gonzalobringas1-art/Dsw2026Ej8/Problema1.cs
+++ ej08-gonzalobringas1-art/Dsw2026Ej8/Problema1.cs
@@ -1,17 +1,4 @@
-/*Crear un método público ObtenerEtiquetaProducto dentro de una clase parcial
-ProductHelper.
-El método debe recibir:
-● long code
-● string description
-● decimal price
-Debe retornar una cadena con el formato:
-[code] description - price
-El precio debe mostrarse en formato moneda.
-Condiciones:
-● La clase ProductHelper debe estar dividida en dos archivos usando partial.
-● En un archivo debe ubicarse el método ObtenerEtiquetaProducto.
- ● En el otro archivo debe ubicarse al menos otro método auxiliar o constante utilizada
-por el método principal. */
+
 
 namespace SistemaProductos
 {
@@ -41,10 +28,7 @@ namespace SistemaProductos
     }
 }
 
-//ProductHelper helper = new ProductHelper();
-//string etiqueta = helper.ObtenerEtiquetaProducto(10254, "Monitor Gamer 24'", 450000.50m);
 
-//Console.WriteLine(etiqueta); 
 
 
 
